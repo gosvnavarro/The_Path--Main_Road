cd658a940e10a73a48e4e06604c6712b9bcba857
gosvn <44584331+gosvnavarro@users.noreply.github.com>
2019-09-11 13:33:23 -0300

    Update PERS_.R

--- PERS_.R
+++ PERS_.R
@@ -1,130 +1,160 @@
-#############################
-###### CÁLCULO DO PERS ######
-####### Script em R #########
-#############################
+######################################
+########## CALCULO DO PERS ###########
+######################################
 
-
-# Carregar a tabela e os pacotes para o ambiente
+# Passo 1 - Carregar a tabela e os pacotes para o ambiente
 library(readr)
 library(dplyr)
 library(formattable)
-teste_varamb_W0 <- read_csv("[caminho_arquivo]")
+ARQUIVO_CALCULO <- read.delim2(file = "arquivo_respostas.csv", header=FALSE)
 
-# Renomear as colunas !!CASO PRECISE!!
-names(teste_varamb_W0) <- c('ID', 'varamb_2', 'varamb_3', 'varamb_4', 'varamb_5', 'varamb_6', 'varamb_7')
+# Passo 2 - Caso precise renomear as colunas seguir o passo a seguir, 
+#           caso não seja necessário seguir para o passo 3
+names(ARQUIVO_CALCULO) <- c('ID', 'varamb_1', 'varamb_2', 'varamb_3', 'varamb_4', 'varamb_5', 
+                            'varamb_6', 'varamb_7')
 
-#Substituir os valores NA por 0
-varambs_W0_2500$varamb1_B[which(is.na(varambs_W0_2500$varamb1_B))] <- 0
-varambs_W0_2500$varamb2_2_B[which(is.na(varambs_W0_2500$varamb2_2_B))] <- 0
-varambs_W0_2500$varamb3_B[which(is.na(varambs_W0_2500$varamb3_B))] <- 0
-varambs_W0_2500$varamb4_B[which(is.na(varambs_W0_2500$varamb4_B))] <- 0
-varambs_W0_2500$varamb5_B[which(is.na(varambs_W0_2500$varamb5_B))] <- 0
-varambs_W0_2500$varamb6_B[which(is.na(varambs_W0_2500$varamb6_B))] <- 0
-varambs_W0_2500$varamb7_2_B[which(is.na(varambs_W0_2500$varamb7_2_B))] <- 0
-varambs_W0_2500$varamb8_B[which(is.na(varambs_W0_2500$varamb8_B))] <- 0
-varambs_W0_2500$varamb9_B[which(is.na(varambs_W0_2500$varamb9_B))] <- 0
+# Passo 3 - Substituir os valores 'NA' por 0
+ARQUIVO_CALCULO$varamb_1[which(is.na(ARQUIVO_CALCULO$varamb_1))] <- 0
+ARQUIVO_CALCULO$varamb_2[which(is.na(ARQUIVO_CALCULO$varamb_2))] <- 0
+ARQUIVO_CALCULO$varamb_3[which(is.na(ARQUIVO_CALCULO$varamb_3))] <- 0
+ARQUIVO_CALCULO$varamb_4[which(is.na(ARQUIVO_CALCULO$varamb_4))] <- 0
+ARQUIVO_CALCULO$varamb_5[which(is.na(ARQUIVO_CALCULO$varamb_5))] <- 0
+ARQUIVO_CALCULO$varamb_6[which(is.na(ARQUIVO_CALCULO$varamb_6))] <- 0
+ARQUIVO_CALCULO$varamb_7[which(is.na(ARQUIVO_CALCULO$varamb_7))] <- 0
+ARQUIVO_CALCULO$varamb_8[which(is.na(ARQUIVO_CALCULO$varamb_8))] <- 0
+ARQUIVO_CALCULO$varamb_9[which(is.na(ARQUIVO_CALCULO$varamb_9))] <- 0
 
-# Os passos abaixo só foram realizados para que a substituição das respostas positivas pelos odds ratios respectivos desse certo
-varambs_W0_2500$varamb1_B[varambs_W0_2500$varamb1_B == 1] <- 2
-varambs_W0_2500$varamb2_2_B[varambs_W0_2500$varamb2_2_B == 1] <- 2
-varambs_W0_2500$varamb3_B[varambs_W0_2500$varamb3_B == 1] <- 2
-varambs_W0_2500$varamb4_B[varambs_W0_2500$varamb4_B == 1] <- 2
-varambs_W0_2500$varamb5_B[varambs_W0_2500$varamb5_B == 1] <- 2
-varambs_W0_2500$varamb6_B[varambs_W0_2500$varamb6_B == 1] <- 2
-varambs_W0_2500$varamb7_2_B[varambs_W0_2500$varamb7_2_B == 1] <- 2
+# Passo 4 - A fim de evitar erros no passo referente aos odds ratio,
+#           os comandos descritos nesse passo são realizados para que 
+#           a substituição das respostas positivas pelos odds ratios respectivos desse certo.
+ARQUIVO_CALCULO$varamb_1[ARQUIVO_CALCULO$varamb_1 == 1] <- 2
+ARQUIVO_CALCULO$varamb_2[ARQUIVO_CALCULO$varamb_2 == 1] <- 2
+ARQUIVO_CALCULO$varamb_3[ARQUIVO_CALCULO$varamb_3 == 1] <- 2
+ARQUIVO_CALCULO$varamb_4[ARQUIVO_CALCULO$varamb_4 == 1] <- 2
+ARQUIVO_CALCULO$varamb_5[ARQUIVO_CALCULO$varamb_5 == 1] <- 2
+ARQUIVO_CALCULO$varamb_6[ARQUIVO_CALCULO$varamb_6 == 1] <- 2
+ARQUIVO_CALCULO$varamb_7[ARQUIVO_CALCULO$varamb_7 == 1] <- 2
+ARQUIVO_CALCULO$varamb_8[ARQUIVO_CALCULO$varamb_8 == 1] <- 2
+ARQUIVO_CALCULO$varamb_9[ARQUIVO_CALCULO$varamb_9 == 1] <- 2
 
-# Checar o tipo de cada coluna
-sapply(varambs_W0_2500, mode)
+# Passo 5 - Checar o tipo de cada coluna
+sapply(ARQUIVO_CALCULO, mode)
 
-# Estipular o OR de cada variavel ambiental para cada individuo
-## para a varamb_1 - Urbanicidade
-for(i in varambs_W0_2500$varamb1_B) {
+# Passo 6 - Estipular o OR de cada variavel ambiental para cada individuo
+# Varamb_1 - Season of birth 
+#            (fazer esse 'for' caso o 'season of birth' tenha sido obtido em conjunto com as outras variaveis)
+for(i in ARQUIVO_CALCULO$varamb_1) {
   if (i == 0){
     print('Valor mantido zero')
   }
   else {
-    varambs_W0_2500$varamb1_B[varambs_W0_2500$varamb1_B == 2] <- 0.54
+    ARQUIVO_CALCULO$varamb_1[ARQUIVO_CALCULO$varamb_1 == 2] <- 0.068
+    print('Valor substituido por 0.068')
+  }
+}
+
+# Varamb_2 - Urbanicidade
+for(i in ARQUIVO_CALCULO$varamb_2) {
+  if (i == 0){
+    print('Valor mantido zero')
+  }
+  else {
+    ARQUIVO_CALCULO$varamb_2[ARQUIVO_CALCULO$varamb_2 == 2] <- 0.54
     print('Valor substituido por 0.54')
   }
 }
 
-## para a varamb_2_2 - Uso de drogas
-for(i in varambs_W0_2500$varamb2_2_B) {
+# Varamb_3 - Uso de drogas
+for(i in ARQUIVO_CALCULO$varamb_3) {
   if (i == 0){
     print('Valor mantido zero')
   }
   else {
-    varambs_W0_2500$varamb2_2_B[varambs_W0_2500$varamb2_2_B == 2] <- 0.56
+    ARQUIVO_CALCULO$varamb_3[ARQUIVO_CALCULO$varamb_3 == 2] <- 0.56
     print('Valor substituido por 0.56')
   }
 }
 
-## para a varamb_3 - Complicações obstétricas e pré-natais
-for(i in varambs_W0_2500$varamb3_B) {
+# Varamb_4 - Idade paterna ao nascimento
+#            (fazer esse 'for' caso o 'paternal age' tenha sido obtido em conjunto com as outras variaveis)
+for(i in ARQUIVO_CALCULO$varamb_4) {
   if (i == 0){
     print('Valor mantido zero')
   }
   else {
-    varambs_W0_2500$varamb3_B[varambs_W0_2500$varamb3_B == 2] <- 0.69
+    ARQUIVO_CALCULO$varamb_4[ARQUIVO_CALCULO$varamb_4 == 2] <- 0.25/0.80
+    print('Valor substituido por 0.25/0.80')
+  }
+}
+
+# Varamb_5 - Obstetric & Pre-natal complications
+for(i in ARQUIVO_CALCULO$varamb_5) {
+  if (i == 0){
+    print('Valor mantido zero')
+  }
+  else {
+    ARQUIVO_CALCULO$varamb_5[ARQUIVO_CALCULO$varamb_5 == 2] <- 0.69
     print('Valor substituido por 0.69')
   }
 }
 
-## para a varamb_4 - Abuso físico
-for(i in varambs_W0_2500$varamb4_B) {
+# Varamb_6 -  Abuso físico
+for(i in ARQUIVO_CALCULO$varamb_6) {
   if (i == 0){
     print('Valor mantido zero')
   }
   else {
-    varambs_W0_2500$varamb4_B[varambs_W0_2500$varamb4_B == 2] <- 1.08
+    ARQUIVO_CALCULO$varamb_6[ARQUIVO_CALCULO$varamb_6 == 2] <- 1.08
     print('Valor substituido por 1.08')
   }
 }
 
-## para a varamb_5 - Abuso sexual
-for(i in varambs_W0_2500$varamb5_B) {
+# Varamb_7 - Abuso sexual
+for(i in ARQUIVO_CALCULO$varamb_7) {
   if (i == 0){
     print('Valor mantido zero')
   }
   else {
-    varambs_W0_2500$varamb5_B[varambs_W0_2500$varamb5_B == 2] <- 0.87
+    ARQUIVO_CALCULO$varamb_7[ARQUIVO_CALCULO$varamb_7 == 2] <- 0.87
     print('Valor substituido por 0.87')
   }
 }
 
-## para a varamb_6 - Negligência
-for(i in varambs_W0_2500$varamb6_B) {
+# Varamb_8 - Negligencia
+for(i in ARQUIVO_CALCULO$varamb_8) {
   if (i == 0){
     print('Valor mantido zero')
   }
   else {
-    varambs_W0_2500$varamb6_B[varambs_W0_2500$varamb6_B == 2] <- 1.06
+    ARQUIVO_CALCULO$varamb_8[ARQUIVO_CALCULO$varamb_8 == 2] <- 1.06
     print('Valor substituido por 1.06')
   }
 }
 
-## para a varamb_7 - Perda e/ou Separação dos Pais
-for(i in varambs_W0_2500$varamb7_2_B) {
+# Varamb_9 - Perda/Separação dos pais
+for(i in ARQUIVO_CALCULO$varamb_9) {
   if (i == 0){
     print('Valor mantido zero')
   }
   else {
-    varambs_W0_2500$varamb7_2_B[varambs_W0_2500$varamb7_2_B == 2] <- 0.53
+    ARQUIVO_CALCULO$varamb_9[ARQUIVO_CALCULO$varamb_9 == 2] <- 0.53
     print('Valor substituido por 0.53')
   }
 }
 
-# Checar o tipo de cada coluna
-sapply(varambs_W0_2500, mode)
+# Passo 7 - Checar o tipo de cada coluna
+sapply(ARQUIVO_CALCULO, mode)
 
-# Criar uma coluna com a soma de todas varambs, resultando assim no PERS (somar todos os valores e dividir por 9)
-varambs_W0_2500$SOMA <- rowSums(cbind(varambs_W0_2500$varamb1_B, varambs_W0_2500$varamb2_2_B, varambs_W0_2500$varamb3_B, 
-                                      varambs_W0_2500$varamb4_B, varambs_W0_2500$varamb5_B, varambs_W0_2500$varamb6_B, 
-                                      varambs_W0_2500$varamb7_2_B, varambs_W0_2500$varamb8_B, varambs_W0_2500$varamb9_B))
+# Passo 8 - Criar uma coluna com a soma de todos os OR, resultando assim no pré-PERS
+ARQUIVO_CALCULO$SOMA <- rowSums(cbind(ARQUIVO_CALCULO$varamb_1, ARQUIVO_CALCULO$varamb_2, 
+                                      ARQUIVO_CALCULO$varamb_3, ARQUIVO_CALCULO$varamb_4, 
+                                      ARQUIVO_CALCULO$varamb_5, ARQUIVO_CALCULO$varamb_6, 
+                                      ARQUIVO_CALCULO$varamb_7, ARQUIVO_CALCULO$varamb_8, 
+                                      ARQUIVO_CALCULO$varamb_9))
 
-# Calcular o PERS
-varambs_W0_2500$PERS <- (varambs_W0_2500$SOMA) / 9
+# Passo 9 - Calcular o PERS
+ARQUIVO_CALCULO$PERS <- (ARQUIVO_CALCULO$SOMA) / 9
 
-# Salvar a tabela
-write.table(varambs_W0_2500, file = '[nome_do_arquivo_final]', sep = "\t", quote = FALSE, col.names = TRUE, row.names = FALSE)
+# Passo 10 - Salvar a tabela
+write.table(ARQUIVO_CALCULO, file = 'arquivo_pers.xlsx', sep = "\t", quote = FALSE, col.names = TRUE, row.names = FALSE)
 
