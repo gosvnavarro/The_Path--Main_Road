aba7a7d7bd4d4592c963727cb75e5ad34b955169
gosvn <44584331+gosvnavarro@users.noreply.github.com>
2019-11-25 12:50:31 -0300

    Update CORRIGIR_PRS.R

--- CORRIGIR_PRS.R
+++ CORRIGIR_PRS.R
@@ -2,32 +2,11 @@
 # PRS CORRIGIDO #
 #################
 
-# O PRS corrigido nada mais e que os resíduos da regressão 
-#   que leva em consideracao o prs e os pcs de interesse
-PRScorrigido <- lm(prs_pcs$PRS ~ prs_pcs$V3 + prs_pcs$V4 + prs_pcs$V5 + prs_pcs$V6 +
-                        prs_pcs$V7 + prs_pcs$V8 + prs_pcs$V9 + prs_pcs$V10 + prs_pcs$V11 + prs_pcs$V12)
+# O PRS corrigido nada mais e que os resíduos de uma regressão linear,
+#   este leva em consideracao o PRS (Best_Score) e os PCs (PCA)
+PRScorrigido <- lm(ARQUIVO$PRS ~ ARQUIVO$PC_1 + ARQUIVO$PC_2 + ARQUIVO$PC_3 + ARQUIVO$PC_4 +
+                        ARQUIVO$PC_5 + ARQUIVO$PC_6 + ARQUIVO$PC_7 + ARQUIVO$PC_8 + ARQUIVO$PC_9 + ARQUIVO$PC_10)
 
-
-PRScorrigido_v2 <- lm(prs_pcs$CAPE ~ prs_pcs$V3 + prs_pcs$V4 + prs_pcs$V5 + prs_pcs$V6 +
-                        prs_pcs$V7 + prs_pcs$V8 + prs_pcs$V9 + prs_pcs$V10 + prs_pcs$V11 + prs_pcs$V12)
-
-PRScorrigido_v3 <- lm(prs_pcs$PRS ~ prs_pcs$PRS + prs_pcs$V3 + prs_pcs$V4 + prs_pcs$V5 + prs_pcs$V6 +
-                        prs_pcs$V7 + prs_pcs$V8 + prs_pcs$V9 + prs_pcs$V10 + prs_pcs$V11 + prs_pcs$V12)
 
 residuos_prs <- residuals(PRScorrigido)
-residuos_prs_v2 <- residuals(PRScorrigido_v2)
-residuos_prs_v3 <- residuals(PRScorrigido_v3)
-
-head(residuos_prs)
-head(residuos_prs_v2)
-head(residuos_prs_v3)
-
-#----
-resultado_cor4 <- cor(correlacao_4, method = "spearman")
-corrplot.mixed(resultado_cor4, lower.col = "black", number.cex = .7, order = "hclust", addrect = 2)
-
-resultado_cor5 <- cor(correlacao_5, method = "spearman")
-corrplot.mixed(resultado_cor5, lower.col = "black", number.cex = .7, order = "hclust", addrect = 2)
-
-resultado_cor6 <- cor(correlacao_6, method = "spearman")
-corrplot.mixed(resultado_cor6, lower.col = "black", number.cex = .7, order = "hclust", addrect = 2)
+View(residuos_prs)
